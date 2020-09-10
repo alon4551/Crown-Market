8ae575bb4e35d507adbcc9fa1abbfa133e3d6cc3
אלון שרייבמן <alon4551@gmail.com>
2020-09-10 13:01:29 +0300

    finishing sign in handling method

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
               id:snapshot.id,
               ...snapshot.data()
             }
-          },()=>console.log(this.state.currentUser))
+          })
           
         });
       }
