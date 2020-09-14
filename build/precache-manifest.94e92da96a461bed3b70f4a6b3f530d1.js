b8f3ce98f171290526a3a629e3172beb148d1d78
אלון שרייבמן <alon4551@gmail.com>
2020-09-14 14:54:15 +0300

    adding files

---
+++ build/precache-manifest.94e92da96a461bed3b70f4a6b3f530d1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5ceb58b381f1519b9f70881629944d",
+    "url": "/alon4551/Crown-Market/index.html"
+  },
+  {
+    "revision": "90c6eda27c620186911b",
+    "url": "/alon4551/Crown-Market/static/css/main.a11a74f5.chunk.css"
+  },
+  {
+    "revision": "289bd536c3f9791a2a43",
+    "url": "/alon4551/Crown-Market/static/js/2.09be5357.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/alon4551/Crown-Market/static/js/2.09be5357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c6eda27c620186911b",
+    "url": "/alon4551/Crown-Market/static/js/main.03ea957e.chunk.js"
+  },
+  {
+    "revision": "afa700a5083a595239e3",
+    "url": "/alon4551/Crown-Market/static/js/runtime-main.1ce9307b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/alon4551/Crown-Market/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/alon4551/Crown-Market/static/media/shopping-bag.30f07071.svg"
+  }
+]);
